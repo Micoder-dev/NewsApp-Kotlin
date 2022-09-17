2de0905aafdb5263702527c4341082ef515613c5
Micoder-dev <micoder.com@gmail.com>
2022-09-17 16:13:31 +0530

    Retrofit Setup

---
+++ app/src/main/java/com/micoder/newsapp/Source.kt
@@ -0,0 +1,6 @@
+package com.micoder.newsapp
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/micoder/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.micoder.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "e0f8cb182e6c40388be01945eec57400"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}

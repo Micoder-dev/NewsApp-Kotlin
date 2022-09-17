20c1b228acd134181a412f1e2c1d60d6df34c7f3
Micoder-dev <micoder.com@gmail.com>
2022-09-17 16:25:37 +0530

    DAO and Article Setup Room

---
+++ app/src/main/java/com/micoder/newsapp/db/ArticleDao.kt
@@ -0,0 +1,19 @@
+package com.micoder.newsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.micoder.newsapp.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles() : LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+}
